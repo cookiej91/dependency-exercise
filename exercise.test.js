0c6e72ea009e160eeeae174a20bb0ad795e8fa82
Joshua <jsc91@live.co.uk>
2019-06-01 13:57:20 +0100

    Refactor to remove strict equal

--- exercise.test.js
+++ exercise.test.js
@@ -1,10 +1,10 @@
 const exercise = require("./exercise");
 
 test("No jobs passed in, the result should be an empty sequence", () => {
-  expect(exercise("")).toStrictEqual([]);
+  expect(exercise("")).toEqual([]);
 });
 
 test("A single job 'a' passed in, the result should be a sequence containing only 'a'", () => {
   let arr = ["a"];
-  expect(exercise(arr)).toStrictEqual(["a"]);
+  expect(exercise(arr)).toEqual(["a"]);
 });
