1694955a06538859091a478085abacd98bd141bf
Joshua <jsc91@live.co.uk>
2019-06-01 15:07:26 +0100

    Add jest-extended and add basic tests

--- exercise.test.js
+++ exercise.test.js
@@ -5,6 +5,13 @@ test("No jobs passed in, the result should be an empty sequence", () => {
 });
 
 test("A single job 'a' passed in, the result should be a sequence containing only 'a'", () => {
-  let arr = ["a"];
-  expect(exercise(arr)).toEqual(["a"]);
+  let collection = "a =>";
+  expect(exercise(collection)).toEqual(["a"]);
+});
+
+test("Three jobs 'a, b, c' passed in, the result should be a sequence containing all three jobs - in no significant order", () => {
+  let collection = `a =>
+b =>
+c =>`;
+  expect(exercise(collection)).toIncludeSameMembers(["a", "b", "c"]);
 });
