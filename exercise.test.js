f46151e1ada901888dae1c58fb51a2f1e54dce38
Joshua <jsc91@live.co.uk>
2019-06-01 11:59:12 +0100

    Add second test to check when a single job is passed in

--- exercise.test.js
+++ exercise.test.js
@@ -3,3 +3,8 @@ const exercise = require("./exercise");
 test("No jobs passed in, the result should be an empty sequence", () => {
   expect(exercise("")).toStrictEqual([]);
 });
+
+test("A single job 'a' passed in, the result should be a sequence containing only 'a'", () => {
+  let arr = ["a"];
+  expect(exercise(arr)).toStrictEqual(["a"]);
+});
