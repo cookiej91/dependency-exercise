014f00286474c97e3b4f66e0db6a2a3a652a1696
Joshua <jsc91@live.co.uk>
2019-06-01 15:52:45 +0100

    Add extended dependency test

--- exercise.test.js
+++ exercise.test.js
@@ -15,3 +15,25 @@ b =>
 c =>`;
   expect(exercise(collection)).toIncludeSameMembers(["a", "b", "c"]);
 });
+
+test("Three jobs 'a, b, c' passed in, the result should be a sequence that positions c before b", () => {
+  let result = exercise(`a =>
+b => c
+c =>`);
+  expect(result).toIncludeSameMembers(["a", "b", "c"]);
+  expect(result.indexOf("c")).toBeLessThan(result.indexOf("b"));
+});
+
+test("Three jobs 'a, b, c, d, e, f' passed in, sorts jobs into dependency order", () => {
+  let result = exercise(`a =>
+b => c
+c => f
+d => a
+e => b
+f =>`);
+  expect(result).toIncludeSameMembers(["a", "b", "c", "d", "e", "f"]);
+  expect(result.indexOf("f")).toBeLessThan(result.indexOf("c"));
+  expect(result.indexOf("c")).toBeLessThan(result.indexOf("b"));
+  expect(result.indexOf("b")).toBeLessThan(result.indexOf("e"));
+  expect(result.indexOf("a")).toBeLessThan(result.indexOf("d"));
+});
